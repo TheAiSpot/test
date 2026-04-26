9ee55f0f3c6d4d8bbfee03587736432ef8b2816b
TheAiSpot <130868706+TheAiSpot@users.noreply.github.com>
2026-04-26 04:00:45 -0700

    Update adoptme_tracker.lua

--- adoptme_tracker.lua
+++ adoptme_tracker.lua
@@ -3,7 +3,7 @@ print("[AT] Script loaded successfully")
 local ok, err = pcall(function()
     print("[AT] Step 1 - variables set")
 
-    local WEBHOOK_URL = "https://deeper-mile-advanced-unnecessary.trycloudflare.com"
+    local WEBHOOK_URL = "https://wrapping-reuters-answered-day.trycloudflare.com"
     local TOKEN       = "IVz1VBLcPIIn3usW3EhkU1_Y9RUT7cJI"
     local USERNAME    = "jon"
     local SCAN_INTERVAL  = 300
